3b86f3deb96e279dfeac3bdedec17725ea9cdf64
Harish064 <87409679+Harish064@users.noreply.github.com>
2021-07-23 10:24:05 +0530

    Made Changes

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,22 @@ import './Table.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator';
 function Table(props) {
-    
-    
-    const paginateData = props.currencyData
-    console.log(paginateData[0])
+    const img_src = ""
+    const currencyData = [...props.currencyData,img_src]
+    const data = props.currencyData.map(x=>{
+        const symbol = x.symbol
+        const img_url = `https://assets.coincap.io/assets/icons/${symbol.toLowerCase()}@2x.png`
+        // console.log(symbol)
+        x.img_src=img_url;
+    })
+    // const imgFormatter = (cell,row) =>{
+    //     // return (<span><img src={row.img_src}/></span>) ;
+    //     console.log(row,cell)
+    // }
+    const paginateData = currencyData
     const paginateColumns = [
         { dataField: 'rank',text: 'Rank',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
-        { dataField: 'image',text: ' ',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
+        //{ dataField: 'img_src',text: '',formatter:{imgFormatter},headerStyle: { backgroundColor: '#215E95', color: 'white'} },
         { dataField: 'name', text: 'Name',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
         { dataField: 'priceUsd', text: 'Price',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
         { dataField: 'marketCapUsd', text: 'Market Cap',headerStyle: { backgroundColor: '#215E95', color: 'white'} },
